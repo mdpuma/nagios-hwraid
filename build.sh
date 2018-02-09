2eefd3435bfe9635ab2fe857a34d905663a1b5d6
pumamd <pumamd@yandex.ru>
2018-02-09 21:19:16 +0200

    * fixes

--- build.sh
+++ build.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+for i in deb rpm; do
+    chmod +x $i/usr/*
+done
+
 # deb package
-fpm -t deb -s dir -C root -n nagios-hwraid -v 0.1 -e
+fpm -t deb -s dir -C deb -n nagios-hwraid -d sudo -v 0.1 -e
 
 # rpm package
-fpm -t rpm -s dir -C root -n nagios-hwraid -v 0.1 -e
+fpm -t rpm -s dir -C rpm -n nagios-hwraid -d sudo -v 0.1 -e
