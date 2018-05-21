b6e2106c67193747baa1ad149d499725d5c7f31c
pumamd <pumamd@yandex.ru>
2018-05-21 10:06:32 +0300

    * updates

--- build.sh
+++ build.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-for i in deb rpm; do
-    chmod +x $i/usr/*
-done
+ver=0.2
 
 # deb package
-fpm -t deb -s dir -C deb -n nagios-hwraid -d sudo -v 0.1 -e
+fpm -t deb -s dir -C deb -n nagios-hwraid \
+	-d sudo -d nagios-nrpe-server -d hpacucli \
+	--post-install after_install -v $ver -e
 
 # rpm package
-fpm -t rpm -s dir -C rpm -n nagios-hwraid -d sudo -v 0.1 -e
+fpm -t rpm -s dir -C rpm -n nagios-hwraid \
+	-d sudo -d nrpe -d hpacucli \
+	--post-install after_install -v $ver -e
